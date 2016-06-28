8029e7482d25d5752f95476c509e8048af8b620a
Sergey Pevnev <s.pevnev@infinnitysolutions.com>
2016-06-28 13:23:17 +0500

    Add message queue examples

---
+++ InfinniPlatform.Northwind/Queues/ExampleMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InfinniPlatform.Northwind.Queues
+{
+    /// <summary>
+    /// Пример тела сообщения.
+    /// </summary>
+    public class ExampleMessage
+    {
+        public ExampleMessage(int someInt, string someText, DateTime someDateTime)
+        {
+            SomeInt = someInt;
+            SomeText = someText;
+            SomeDateTime = someDateTime;
+        }
+
+        public int SomeInt { get; set; }
+
+        public string SomeText { get; set; }
+
+        public DateTime SomeDateTime { get; set; }
+
+        public override string ToString()
+        {
+            return $"{SomeInt}, {SomeText}, {SomeDateTime.ToString("s")}";
+        }
+    }
+}
---
+++ InfinniPlatform.Northwind/Queues/TaskConsumer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using InfinniPlatform.Sdk.Dynamic;
+using InfinniPlatform.Sdk.Queues;
+using InfinniPlatform.Sdk.Queues.Consumers;
+
+namespace InfinniPlatform.Northwind.Queues
+{
+    /// <summary>
+    /// Пример потребителя сообщений из очереди задач.
+    /// </summary>
+    public class TaskConsumerOne : TaskConsumerBase<ExampleMessage>
+    {
+        protected override async Task Consume(Message<ExampleMessage> message)
+        {
+            await Task.Run(() => Console.WriteLine($"{nameof(TaskConsumerOne)} recieved a message {message.Body}."));
+        }
+    }
+
+
+    /// <summary>
+    /// Пример потребителя сообщений из очереди задач.
+    /// </summary>
+    public class TaskConsumerTwo : TaskConsumerBase<ExampleMessage>
+    {
+        protected override async Task Consume(Message<ExampleMessage> message)
+        {
+            await Task.Run(() => Console.WriteLine($"{nameof(TaskConsumerTwo)} recieved a message {message.Body}."));
+        }
+    }
+
+
+    /// <summary>
+    /// Пример потребителя сообщений из широковещательной очереди.
+    /// </summary>
+    [QueueName("DynamicQueue")]
+    public class BroadcastConsumerOne : BroadcastConsumerBase<DynamicWrapper>
+    {
+        protected override async Task Consume(Message<DynamicWrapper> message)
+        {
+            await Task.Run(() => Console.WriteLine($"{nameof(BroadcastConsumerOne)} recieved a message {message.Body["Example"]}."));
+        }
+    }
+
+
+    /// <summary>
+    /// Пример потребителя сообщений из широковещательной очереди.
+    /// </summary>
+    [QueueName("DynamicQueue")]
+    public class BroadcastConsumerTwo : BroadcastConsumerBase<DynamicWrapper>
+    {
+        protected override async Task Consume(Message<DynamicWrapper> message)
+        {
+            await Task.Run(() => Console.WriteLine($"{nameof(BroadcastConsumerTwo)} recieved a message {message.Body["Example"]}."));
+        }
+    }
+}
