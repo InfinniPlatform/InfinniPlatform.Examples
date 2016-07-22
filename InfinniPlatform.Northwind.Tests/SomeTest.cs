50e0bf926bc20486eebdca46b9ef3bece77dd425
AlexMAS <as.mezhov@gmail.com>
2016-07-22 15:10:46 +0500

    Add fake test

---
+++ InfinniPlatform.Northwind.Tests/SomeTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace InfinniPlatform.Northwind.Tests
+{
+    [TestFixture("UnitTest")]
+    public class SomeTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // Given
+
+            // When
+
+            // Then
+        }
+    }
+}
