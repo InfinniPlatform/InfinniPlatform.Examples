e556b957a9f86a2359ca10211dcda78165ddb91c
AlexMAS <as.mezhov@gmail.com>
2016-09-13 10:20:07 +0500

    Update examples

--- InfinniPlatform.Northwind/Scheduler/SomeJobInfoSource.cs
+++ InfinniPlatform.Northwind/Scheduler/SomeJobInfoSource.cs
@@ -8,14 +8,16 @@ namespace InfinniPlatform.Northwind.Scheduler
     /// <summary>
     /// Пример источника заданий.
     /// </summary>
-    public class JobSource : IJobInfoSource
+    public class SomeJobInfoSource : IJobInfoSource
     {
         public Task<IEnumerable<IJobInfo>> GetJobs(IJobInfoFactory factory)
         {
             var jobs = new[]
                        {
-                           // Задание будет выполняться ежедневно в 10:35 с помощью обработчика SomeJobHandler
-                           factory.CreateJobInfo<SomeJobHandler>("SomeJob", b => b.CronExpression(e => e.AtHourAndMinuteDaily(10, 35)))
+                           // Задание будет выполняться каждую минуту
+                           // с помощью обработчика SomeJobHandler
+                           factory.CreateJobInfo<SomeJobHandler>("SomeJob",
+                               b => b.CronExpression(e => e.Seconds(m => m.Each(0))))
                        };
 
             return Task.FromResult<IEnumerable<IJobInfo>>(jobs);
