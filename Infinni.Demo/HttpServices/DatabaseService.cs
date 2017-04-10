b9967979d8057cd6eccbf97b316dc5a7310ad9cd
Unknown <larockarocka@hotmail.com>
2017-04-11 00:29:28 +0500

    Add demo services

---
+++ Infinni.Demo/HttpServices/DatabaseService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+using InfinniPlatform.DocumentStorage.Contract;
+using InfinniPlatform.Sdk.Http.Services;
+
+namespace Infinni.Demo.HttpServices
+{
+    public class DatabaseService : IHttpService
+    {
+        public DatabaseService(IDocumentStorageFactory storageFactory)
+        {
+            _products = storageFactory.GetStorage("Products");
+        }
+
+        private readonly IDocumentStorage _products;
+
+        public void Load(IHttpServiceBuilder builder)
+        {
+            builder.Get["Products"] = Products;
+        }
+
+        private async Task<object> Products(IHttpRequest httpRequest)
+        {
+            var productList = await _products.Find()
+                                             .Limit(20)
+                                             .ToListAsync();
+
+            return new JsonHttpResponse(productList);
+        }
+    }
+}
---
+++ Infinni.Demo/IoC/DemoModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+using InfinniPlatform.Sdk.Http.Services;
+using InfinniPlatform.Sdk.IoC;
+
+namespace Infinni.Demo.IoC
+{
+    public class DemoModule :IContainerModule
+    {
+        public void Load(IContainerBuilder builder)
+        {
+            builder.RegisterHttpServices(typeof(DemoModule).GetTypeInfo().Assembly);
+        }
+    }
+}
