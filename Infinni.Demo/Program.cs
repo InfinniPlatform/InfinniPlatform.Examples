ba91c0f9fdb180da2ba11d96005f0ffd32e7e787
Sergey Pevnev <larockarocka@hotmail.com>
2017-04-10 23:48:23 +0500

    Fix startup

--- Infinni.Demo/Program.cs
+++ Infinni.Demo/Program.cs
@@ -9,9 +9,9 @@ namespace Infinni.Demo
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                    .UseUrls("http://0.0.0.0:5000")    // workaround
                     .UseKestrel()
                     .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseIISIntegration()
                     .UseStartup<Startup>()
                     .Build();
 
--- Infinni.Demo/Startup.cs
+++ Infinni.Demo/Startup.cs
@@ -1,28 +1,46 @@
 ﻿using System;
 
+using InfinniPlatform.Core.Http.Middlewares;
 using InfinniPlatform.Extensions;
 using InfinniPlatform.Sdk.IoC;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Infinni.Demo
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(env.ContentRootPath)
+                    .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            var serviceProvider = services.AddDocumentStorage()
+            var serviceProvider = services.AddAuth()
+                                          .AddDocumentStorage()
                                           .AddLog4NetAdapter()
                                           .BuildProvider();
 
             return serviceProvider;
         }
 
-        public void Configure(IApplicationBuilder app, IContainerResolver resolver, IApplicationLifetime lifetime)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IContainerResolver resolver)
         {
-            app.UseInfinniMiddlewares(resolver, lifetime);
+            loggerFactory.AddConsole();
+            app.UseStaticFiles();
+
+            app.UseInfinniMiddlewares(resolver);
         }
     }
 }
