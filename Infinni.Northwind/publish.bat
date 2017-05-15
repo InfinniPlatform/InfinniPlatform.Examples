4e6912e0b9b4e1d6c3937ecfa3ed9df864e51bca
Sergey Pevnev <larockarocka@hotmail.com>
2017-05-15 08:57:42 +0500

    Remove project name from publish script

--- Infinni.Northwind/publish.bat
+++ Infinni.Northwind/publish.bat
@@ -1,3 +1,3 @@
-dotnet publish -c Release -r win7-x64 -o bin/win7 Infinni.Northwind.csproj
-dotnet publish -c Release -r win81-x64 -o bin/win81 Infinni.Northwind.csproj
-dotnet publish -c Release -r win10-x64 -o bin/win10 Infinni.Northwind.csproj
+dotnet publish -c Release -r win7-x64 -o bin/win7
+dotnet publish -c Release -r win81-x64 -o bin/win81
+dotnet publish -c Release -r win10-x64 -o bin/win10
